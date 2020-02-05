330ddaec578ada000e2b48c4edf0afad119e4c89
WelderMileu <welderfelix21@gmail.com>
2020-02-04 21:14:09 -0300

    Alterando uma função javascript

--- js/temas.js
+++ js/temas.js
@@ -8,14 +8,12 @@ import { $ } from './selectors.js';
 const actived = () => {
 	$('body').classList.add("bg-dark");
 	$('.list').classList.add("text-light"); 
-	$('h1').classList.add("text-light");
 }
 
 // Rempovendo nossos estilos do tema.
 const inactived = () => {
 	$('body').classList.remove("bg-dark");
 	$('.list').classList.remove("text-light");
-	$('h1').classList.remove("text-light");
 }
 
 export { actived, inactived };
